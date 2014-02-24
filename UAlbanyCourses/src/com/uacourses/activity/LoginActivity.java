2ce2df8ff0afdf997fba703664c46080191f6337
Nilangshus <Nilangshudas2010@gmail.com>
2014-02-23 20:04:16 -0500

    session implemented on first page

--- UAlbanyCourses/src/com/uacourses/activity/LoginActivity.java
+++ UAlbanyCourses/src/com/uacourses/activity/LoginActivity.java
@@ -5,8 +5,11 @@ import com.uacourses.activity.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -16,16 +19,28 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends UACoursesActivity {
+	
+	
+	public static final String FILE = "Login.txt";
+    private SharedPreferences sprefs;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Log.d("notify", "on create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        addLoginButton();
+        sprefs = getSharedPreferences(FILE, Context.MODE_PRIVATE);
+    	if(sprefs.contains("username")
+    			&& sprefs.contains("password"))
+		{
+    		gotoDetails();
+		} else {
+			addLoginButton();
+		}
     }
 
-
-   
     
     private void addLoginButton() {
     	
@@ -38,10 +53,13 @@ public class LoginActivity extends UACoursesActivity {
 				
 				EditText username = (EditText)findViewById(R.id.username);
 		    	EditText password = (EditText)findViewById(R.id.password);
-				if(username.getText().toString().equals("UAlbany") && password.getText().toString().equals("UAlbany456"))
+		    	LoginActivity.this.username = username.getText().toString();
+		    	LoginActivity.this.password = password.getText().toString();
+		    	if(LoginActivity.this.username.equals("UAlbany") && LoginActivity.this.password.equals("UAlbany456"))
 		    	{
-		    		Intent intent = new Intent(getApplicationContext(),DetailsActivity.class);
-		    		startActivity(intent);
+		    		saveDataInPrefs(LoginActivity.this.username, LoginActivity.this.password);
+					gotoDetails();
+		    		
 		    	} else {
 		    		Toast toast = Toast.makeText(getApplicationContext(), "Invalid username/ password!!!",
 		    						Toast.LENGTH_SHORT);
@@ -52,5 +70,21 @@ public class LoginActivity extends UACoursesActivity {
 		});
     	
     }
+    
+    public void gotoDetails()
+    {
+    	Intent intent = new Intent(getApplicationContext(),DetailsActivity.class);
+		startActivity(intent);
+    }
+    
+    public void saveDataInPrefs(String username, String password)
+    {
+    	Editor editor = sprefs.edit();
+    	editor.putString("username", username);
+    	editor.putString("password", password);
+    	editor.commit();
+    	
+    }
+    
     
 }
